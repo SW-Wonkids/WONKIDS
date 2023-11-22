76cdda39ae58dc582aa26b5e94bf88695a2f9c05
임지현 <acubens03@gmail.com>
2023-11-23 00:43:36 +0900

    [fix] poll vue and profile vue

--- final-pjt-front/wonkids/src/views/PollView.vue
+++ final-pjt-front/wonkids/src/views/PollView.vue
@@ -2,14 +2,19 @@
   <div>
     <h1>poll index</h1>
     <p>유형검사를 시작해봅시다!</p>
-      <form @submit.prevent="store.logOut">
-          <input type="submit" value="logOut">
+      <form @submit.prevent="goPollQuestions">
+          <input type="submit" value="유형 검사 시작하기">
       </form>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+const goPollQuestions = () => {
+  router.push({ name: 'poll-questions' })
+}
 </script>
 
 <style scoped>
