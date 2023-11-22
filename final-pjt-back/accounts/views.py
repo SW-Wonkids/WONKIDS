29aaa754bf8918f7586a2cd68a21e06ed70963bb
bo kyung kim <rapsparrow99@naver.com>
2023-11-22 21:46:59 +0900

    [feat] profile page

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -11,11 +11,13 @@ from django.shortcuts import get_object_or_404
 
 @api_view(['GET'])
 def getuser(request):
-  pass
-    # print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
-    # print(request)
-    # user = get_object_or_404(get_user_model(), username=request.user)
-    # serializer = CustomUserSerializer(user)
-    # # if serializer.is_valid(raise_exception=True):
-    # #     serializer.save()
-    # return Response(serializer.data, status=status.HTTP_200_OK) 
+
+  print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+  print(request.user)
+  user = get_object_or_404(get_user_model(), username=request.user)
+  serializer = CustomUserSerializer(user)
+  print('ser', serializer.data)
+  # if serializer.is_valid(raise_exception=True):
+  #   serializer.save()
+  print('!!!!!!!!!!!!!!!!!!!!')
+  return Response(serializer.data, status=status.HTTP_200_OK) 
