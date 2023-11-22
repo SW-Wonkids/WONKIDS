39894dd8a3410ce5985dcfc3d5f05c1bc6ea764d
bo kyung kim <rapsparrow99@naver.com>
2023-11-22 21:46:59 +0900

    [feat] profile page

--- final-pjt-front/wonkids/src/views/ProfileView.vue
+++ final-pjt-front/wonkids/src/views/ProfileView.vue
@@ -1,23 +1,26 @@
 <template>
   <div>
-    <Profile 
-    v-for="user in store.users"
-    :key="user.id"
-    :user="user"/>
+    <h1>{{ userinfo.username }} 님의 프로필 페이지</h1>
+    <hr>  
+    <p> 나의 결과는!! {{ userinfo.category }}</p>
+    <p> 나의 아이디 : {{ userinfo.id }}</p>
   </div>
+  
 </template>
 
 <script setup>
 
-import Profile from '@/components/Profile.vue'
 import { onMounted } from 'vue'
 import { useProfileStore } from '@/stores/profile'
 
 const store = useProfileStore()
 
+const userinfo = store.userinfo
+
 onMounted(() => {
     store.getProfile()
 })
+
 </script>
 
 <style scoped>
