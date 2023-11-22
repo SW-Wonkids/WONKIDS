4a131b71a2c4f93b08ebf5c6ec607c1f10a82da1
bo kyung kim <rapsparrow99@naver.com>
2023-11-22 09:23:56 +0900

    [feat]connect User in vue

--- final-pjt-front/wonkids/src/views/ProfileView.vue
+++ final-pjt-front/wonkids/src/views/ProfileView.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
     <h1>profile</h1>
+    <p>{{ users }}</p>
 
   </div>
 </template>
 
-<script setup>
-
+<script>
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:5173';
+// User 목록 가져오기, this: 현재 컴포넌트의 인스턴스
+export default {
+    data() {
+        return {
+            users: [],  //User 정보 저장할 배열 
+        };
+    },
+    mounted() {
+        this.fetchUsers();
+    },
+    methods: {
+        fetchUsers() {
+            axios.get('api/users/')
+                .then(res => {
+                    this.users = res.data
+                })
+                .catch((error) => {
+                    console.error('error is what:', error)
+                });
+        },
+    },
+};
 </script>
 
 <style scoped>
