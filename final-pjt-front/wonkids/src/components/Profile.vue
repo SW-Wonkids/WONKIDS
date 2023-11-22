6d22f48db0acc2c56210fd6172336a18ea21e94b
bo kyung kim <rapsparrow99@naver.com>
2023-11-22 15:33:19 +0900

    [merge]

--- final-pjt-front/wonkids/src/components/Profile.vue
+++ final-pjt-front/wonkids/src/components/Profile.vue
@@ -1,13 +1,10 @@
 <template>
   
   <div>
-
-
     <h1>{{ user.username }} 님의 프로필 페이지</h1>
     <hr>  
     <p> 나의 결과는!! 카테고리 입력될 부분{{ user.category }}</p>
     <p> 나의 아이디 : {{ user.id }}</p>
-
 
   </div>
 </template>
