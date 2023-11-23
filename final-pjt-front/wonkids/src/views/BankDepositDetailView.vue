9cbae998d100a95dc319d5d282346b1a4bba50be
bo kyung kim <rapsparrow99@naver.com>
2023-11-24 01:55:28 +0900

    [fix] css layout

--- final-pjt-front/wonkids/src/views/BankDepositDetailView.vue
+++ final-pjt-front/wonkids/src/views/BankDepositDetailView.vue
@@ -1,8 +1,13 @@
 <!--BankListDepositView > BankListDeposit > BankDeposit > BankDepositDetailView -->
 <template>
-  <div>
-    <p class="topic">구체적으로 이 정기예금 금융상품에 대해 알아보자!</p>
+  <div class="koreanfont">
+    
     <div v-if="bank">
+    <p style="font-size: 20px; text-align: center; margin-top: 30px;">구체적으로 <strong>{{ bank.fin_prdt_nm }}</strong> 에 대해 알아보자!</p>
+
+      <div class="container">
+      <div class="card">
+      <div class="card-body">
       <p>공시 제출월[YYYYMM] : {{ bank.dcls_month }}</p>
       <p>금융회사 명 : {{ bank.kor_co_nm }}</p>
       <p>상품명 : {{ bank.fin_prdt_nm }}</p>
@@ -12,6 +17,9 @@
       <RouterLink :to="{name: 'banklist_deposit'}">
       [BACK]
       </RouterLink>
+    </div>
+    </div>
+    </div>
     </div>
 
   </div>
@@ -41,8 +49,21 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.topic {
-  text-decoration: rgb(53,106,188)  wavy underline;
+.card {
+  width: 50%;
+  border: 2px solid rgb(53,106,188);
+  border-radius: 2%;
+  margin-top: 30px;
+  margin-left: 55px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+a{
+  text-decoration: none;
 }
 </style>
 
--- final-pjt-front/wonkids/src/views/BankListDepositView.vue
+++ final-pjt-front/wonkids/src/views/BankListDepositView.vue
@@ -1,8 +1,8 @@
 <template>
   <!--BankListDepositView > BankListDeposit > BankDeposit 정기예금 vue page -->
   <div>
-    <RouterLink :to="{name: 'banktotal'}">전체 금융상품 확인하기</RouterLink>    |  
-    <RouterLink :to="{name: 'banklist_savings'}">적금(SAVINGS) 금융상품은 여기서 확인해보세요!</RouterLink>  
+    <RouterLink :to="{name: 'banktotal'}">전체 금융상품 확인하기</RouterLink><br>  
+    <RouterLink :to="{name: 'banklist_savings'}">정기적금(SAVINGS) 금융상품은 여기서 확인해보세요!</RouterLink>  
     <hr>
     <BankListDeposit />
 
@@ -22,5 +22,7 @@ onMounted(() => {
 </script>
 
 <style scoped>
-
+a {
+  text-decoration: none;
+}
 </style>  
