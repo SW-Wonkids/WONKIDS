f6e2cd21c9cba12d56fc24f91a772a1ee1d6dcd6
bo kyung kim <rapsparrow99@naver.com>
2023-11-23 11:42:38 +0900

    [style] tryout change font

--- final-pjt-front/wonkids/src/views/HomeView.vue
+++ final-pjt-front/wonkids/src/views/HomeView.vue
@@ -39,6 +39,16 @@
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="icon-bar">
+  <a class="active" href="#"><i class="fa fa-home"></i></a>
+  <a href="#"><i class="fa fa-search"></i></a>
+  <a href="#"><i class="fa fa-envelope"></i></a>
+  <a href="#"><i class="fa fa-globe"></i></a>
+</div> -->
+
 </template>
 
 <script>
@@ -61,4 +71,26 @@
 </script>
 
 <style scoped>
+.icon-bar {
+  width: 50px; /* Set a specific width */
+  background-color: #555; /* Dark-grey background */
+}
+
+.icon-bar a {
+  display: block; /* Make the links appear below each other instead of side-by-side */
+  text-align: center; /* Center-align text */
+  padding: 16px; /* Add some padding */
+  transition: all 0.3s ease; /* Add transition for hover effects */
+  color: white; /* White text color */
+  font-size: 20px; /* Increased font-size */
+}
+
+.icon-bar a:hover {
+  background-color: #000; /* Add a hover color */
+}
+
+.active {
+  background-color: #04AA6D; /* Add an active/current color */
+}
+
 </style>
