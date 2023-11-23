4d58d59d2439513f7bd3f84f5a13f401b671ff2f
임지현 <acubens03@gmail.com>
2023-11-23 11:41:16 +0900

    [fix] poll get function

--- final-pjt-front/wonkids/src/views/ProfileView.vue
+++ final-pjt-front/wonkids/src/views/ProfileView.vue
@@ -1,6 +1,7 @@
 <template>
     <h1>{{ userinfo.username }} 님의 프로필 페이지</h1>
     <hr>  
+    hello {{ userinfo.category }}
     <div v-if="userinfo.category">
       <p>나의 결과는!! {{ userinfo.category }}</p>
       <p>결과에 따른 추천 상품 목록</p>
@@ -40,7 +41,8 @@
         <input type="submit" value="유형검사 다시 하기">
       </form>
     </div>
-
+  </div>
+  
     <div v-else>
       <p>나의 유형은? 지금 유형검사에 참여하여 나의 유형도 알고</p>
       <p>추천 상품 목록도 확인해보세요!</p>
@@ -48,13 +50,14 @@
           <input type="submit" value="유형검사 하러 가기">
         </form>
     </div>
+
     <p>나의 아이디 : {{ userinfo.username }}</p>
     <p>나의 이메일 : {{ userinfo.email }}</p>
     <p>나의 나이: {{ userinfo.age }}</p>
     <p>나의 학교: {{ userinfo.school }}</p>
     <p>나의 학년: {{ userinfo.grade }}</p>
     <p>나의 반: {{ userinfo.classnum }}</p>
-  </div>
+  
   
 </template>
 
