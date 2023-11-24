cde20ef1d60a92e99cf4eef045d4104742f727a3
bo kyung kim <rapsparrow99@naver.com>
2023-11-24 11:52:11 +0900

    [feat] home and 404 page

--- final-pjt-front/wonkids/src/views/HomeView.vue
+++ final-pjt-front/wonkids/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <!-- bootstrap text code -->
 <template>
   <div class="koreanfont" style="margin-left: 55px; margin-right: 55px;">
-    <RouterLink :to="{name: 'home'}"><img src="@/assets/wonkids.png" class="d-block w-100" style="max-width: 500px; height: auto; margin: auto; display: block; width: 60%"></RouterLink>
-    
-    <div id="carouselExampleCaptions" class="carousel slide">
+    <RouterLink :to="{name: 'home'}"><img src="@/assets/wonkids.png" class="d-block w-100" style="max-width: 400px; height: auto; margin: auto; display: block; width: 60%"></RouterLink>
+    <div class="container">
+    <div id="carouselExampleCaptions" class="carousel slide" style="max-width: 800px">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -12,7 +12,7 @@
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="@/assets/exchange.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="card carousel-caption d-none d-md-block">
           <h5>예금 & 적금 금융상품의 금리를 비교해볼 수 있어요!</h5>
           <p>금융 상품을 금융회사를 기준으로 필터링해 금리 정보를 비교할 수 있어요!<br>
           각 상품을 클릭하면 상세 페이지로 이동해서 가입방법, 우대조건을 알아봅시다.</p>
@@ -20,7 +20,7 @@
       </div>
       <div class="carousel-item">
         <img src="@/assets/poll.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="card carousel-caption d-none d-md-block">
           <h5>설문조사를 활용한 금융상품 추천 서비스</h5>
           <p>포켓몬을 활용한 미니 설문조사를 통해 나의 성향을 파악하고 필요한 금융상품을 추천받을 수 있는 서비스입니다. 
             
@@ -29,7 +29,7 @@
       </div>
       <div class="carousel-item">
         <img src="@/assets/bank.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
+        <div class="card carousel-caption d-none d-md-block">
           <h5>환율 계산기</h5>
           <p>환율을 계산할 수 있는 계산기가 있어요.<br> 해외에서 내가 가진 돈이 얼마인지 함께 계산해봐요</p>
         </div>
@@ -43,6 +43,7 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Next</span>
     </button>
+  </div>
   </div>
   </div>
 
@@ -89,5 +90,19 @@
 .active {
   background-color: #04AA6D; /* Add an active/current color */
 }
+.container {
+  display: flex;
+  flex-direction:column;
+  align-content: center;
+  align-items: center;
+  text-align: center;
+}
 
+p {
+  color:black;
+}
+h5 {
+  color: black; 
+  font-weight: bolder;
+}
 </style>
--- final-pjt-front/wonkids/src/views/NotFoundView.vue
+++ final-pjt-front/wonkids/src/views/NotFoundView.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <h1>정상적인 접근이 아닙니다!</h1>
-        <p>이전 페이지로 돌아가주세요!</p>
+<div class="container">
+    <div class="background koreanfont">
+        <h1 style="margin-top: 35px; margin-bottom: 5px; font-weight: 800;">404 ERROR</h1>
+        <h1>정상적인 접근이 아닙니다!&#x1F603;</h1>
+        <p style="margin-top: 10px;">이전 페이지로 돌아가주세요!	&#129505;</p>
     </div>
+</div>
 </template>
 
 <script setup>
@@ -10,5 +13,26 @@
 </script>
 
 <style scoped>
+.background {
+    background-image: url('@/assets/sleep.png');
+    height: 90vh;
+    border-radius: 45%;
+    width: 70%;
+        
+}
 
+h1, p {
+    color: white;
+    font-weight: 100;
+    text-align: center;
+    border-top: 50px;
+    margin-top: 50px;
+}
+.container {
+    display: flex;
+    flex-direction:column;
+    align-content: center;
+    align-items: center;
+    text-align: center;
+}
 </style>
